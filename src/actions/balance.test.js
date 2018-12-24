7f20f4c05e62b723a10375c493de5b5daa86548f
Mongkolpoj <mongkolpoj@gmail.com>
2018-12-24 07:30:37 +0700

    first commit

---
+++ src/actions/balance.test.js
@@ -0,0 +1,26 @@
+import * as constants from "./constants";
+import * as actions from "./balance";
+
+it("should create an actoin to set the balance", () => {
+  const balance = 0;
+
+  const expectedAction = { type: constants.SET_BALANCE, balance };
+
+  expect(actions.setBalance(balance)).toEqual(expectedAction);
+});
+
+it("should create an action to deposit into the balance", () => {
+  const deposit = 10;
+
+  const expectedAction = { type: constants.DEPOSIT, deposit };
+
+  expect(actions.deposit(deposit)).toEqual(expectedAction);
+});
+
+it("should creates an action to withdraw from the balance", () => {
+  const withdraw = 10;
+
+  const expectedAction = { type: constants.WITHDRAW, withdraw };
+
+  expect(actions.withdraw(withdraw)).toEqual(expectedAction);
+});
